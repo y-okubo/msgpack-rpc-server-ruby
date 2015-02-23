fe30d2f9a65df0b81bc166bce990e08292a389ce
Yuki Okubo <y-okubo@nekojarashi.com>
2015-02-23 17:23:56 +0900

    Update

--- server.rb
+++ server.rb
@@ -3,23 +3,47 @@ require 'msgpack/rpc'
 # RPCメソッドを実装したクラス
 class Server
 
-  # helloメソッドを実装
   def hello
     return "Hello, World!"
   end
 
-  def hello2
-    return ["AAAA", "BBB日本語"]
+  def method_with_argument(a)
+    puts "Object Type: #{a.class.to_s}"
+    puts "Object value: #{a}"
+    puts ""
   end
 
-  # RPCの遅延リターン
+  def method_return_fixnum
+    return 100000
+  end
+
+  def method_return_bignum
+    return 100000 * 100000
+  end
+
+  def method_return_string
+    return 'ABC'
+  end
+
+  def method_return_boolean
+    return true
+  end
+
+  def method_return_array
+    return ['A', 'B' , 100]
+  end
+
+  def method_return_hash
+    return {:a => 'aaa', 'b' => 'bbb', 1 => 1000}
+  end
+
   def async_hello
     # MessagePack::RPC::AsyncResult のインスタンスを返すと遅延リターンに
     as = MessagePack::RPC::AsyncResult.new
 
     Thread.new do
       # たとえばスレッドプールに投入して非同期でタスクを実行し...
-      sleep 1
+      sleep 10
       # ...処理が終わったら結果を返す
       as.result "ok."
     end
